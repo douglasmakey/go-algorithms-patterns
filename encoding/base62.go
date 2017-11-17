ade197d6bf56e7805d1a0d15e58fae8a4993a38c
Douglas Makey Mendez Molero <douglasmakey@gmail.com>
2017-11-17 15:24:13 -0200

    Update base62.go

--- encoding/base62.go
+++ encoding/base62.go
@@ -1,62 +1,42 @@
 package main
 
 import (
-	"bytes"
-	"math"
 	"fmt"
-	"github.com/douglasmakey/golang-algorithms-/utils"
-	"time"
+	"strings"
 )
 
 // All characters
-const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	length   = int64(len(alphabet))
+)
 
-// Convert number to base62
-func Encode(n int) string {
-	defer utils.TimeTrack(time.Now(), "Encode")
-
+// Encode number to base62.
+func Encode(n int64) string {
 	if n == 0 {
 		return string(alphabet[0])
 	}
 
-	chars := make([]byte, 0)
-
-	length := len(alphabet)
-
-	for n > 0 {
-		r := n / length
-		remainder := n % length
-		chars = append(chars, alphabet[remainder])
-		n = r
+	s := ""
+	for ; n > 0; n = n / length {
+		s = string(alphabet[n%length]) + s
 	}
-
-	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
-		chars[i], chars[j] = chars[j], chars[i]
-	}
-
-	return string(chars)
+	return s
 }
 
-// convert base62 token to int
-func Decode(token string) int {
-	defer utils.TimeTrack(time.Now(), "Decode")
-
-	n := 0
-	idx := 0.0
-	chars := []byte(alphabet)
-
-	charsLength := float64(len(chars))
-	tokenLength := float64(len(token))
-
-	for _, c := range []byte(token) {
-		power := tokenLength - (idx + 1)
-		ind := bytes.IndexByte(chars, c)
-		n += ind * int(math.Pow(charsLength, power))
-		idx++
+// Decode converts a base62 token to int.
+func Decode(key string) (int64, error) {
+	var n int64
+	for _, c := range []byte(key) {
+		i := strings.IndexByte(alphabet, c)
+		if i < 0 {
+			return 0, fmt.Errorf("unexpected character %c in base62 literal", c)
+		}
+		n = length*n + int64(i)
 	}
-
-	return n
+	return n, nil
 }
+
 
 func main()  {
 	encode := Encode(1600000000)
